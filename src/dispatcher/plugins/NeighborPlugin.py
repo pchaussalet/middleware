5625902b68ef24852235e9e08c78bac382b23b2f
Jakub Klama <jakub@ixsystems.com>
2016-09-20 02:30:23 +0200

    Add basic neighbor discovery plugin.

---
+++ src/dispatcher/plugins/NeighborPlugin.py
@@ -0,0 +1,66 @@
+#
+# Copyright 2016 iXsystems, Inc.
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+#####################################################################
+
+from task import Provider, query
+from freenas.dispatcher.rpc import generator
+
+
+KNOWN_SERVICES = ['freenas', 'ssh']
+
+
+class NeighborProvider(Provider):
+    @query('neighbor')
+    @generator
+    def query(self):
+        for regtype in KNOWN_SERVICES:
+            yield from self.dispatcher.call_sync('neighbord.discovery.find', regtype)
+
+
+def _init(dispatcher, plugin):
+    plugin.register_schema_definition('neighbor', {
+        'type': 'object',
+        'additionalProperties': False,
+        'properties': {
+            'id': {'type': 'string'},
+            'hostname': {'type': 'string'},
+            'description': {'type': ['string', 'null']},
+            'type': {'$ref': 'neighbod-type'},
+            'source': {'type': 'string'},
+            'address': {'type': 'string'},
+            'online': {'type': 'boolean'},
+            'properties': {
+                'type': 'object'
+            }
+        }
+    })
+
+    plugin.register_schema_definition('neighbor-type', {
+        'type': 'string',
+        'enum': KNOWN_SERVICES
+    })
+
+    plugin.register_provider('neighbor', NeighborProvider)
