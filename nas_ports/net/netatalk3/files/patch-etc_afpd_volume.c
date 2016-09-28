90f7e17c11c8e7a4309ab7f896831d5ed0418a1e
Suraj Ravichandran <suraj@ixsystems.com>
2016-09-27 17:18:17 -0700

    Update netatalk to 3.1.10 in nas_ports
    While here remove uncecessary patch files and ammend some other appropriately.
    Also update pkg-list to adapt to that of netatalk 3.1.10
    
    Ticket: #17483

--- nas_ports/net/netatalk3/files/patch-etc_afpd_volume.c
+++ nas_ports/net/netatalk3/files/patch-etc_afpd_volume.c
@@ -1,14 +1,15 @@
---- etc/afpd/volume.c.orig	2016-07-20 13:19:58 UTC
-+++ etc/afpd/volume.c
-@@ -380,7 +380,11 @@ static int getvolparams(const AFPObj *ob
-                         ashort |= VOLPBIT_ATTR_UNIXPRIV;
-                     if (vol->v_flags & AFPVOL_TM)
+--- etc/afpd/volume.c.orig	2016-09-27 18:59:31.340571000 -0700
++++ etc/afpd/volume.c	2016-09-27 19:01:47.878600000 -0700
+@@ -385,10 +385,10 @@
                          ashort |= VOLPBIT_ATTR_TM;
-+#ifdef HAVE_LDAP
+ #ifdef HAVE_LDAP
                      if (!ldap_config_valid || vol->v_flags & AFPVOL_NONETIDS)
-+#else
+-                        ashort |= VOLPBIT_ATTR_NONETIDS;
+ #else
+-                    ashort |= VOLPBIT_ATTR_NONETIDS;
 +                    if (vol->v_flags & AFPVOL_NONETIDS)
-+#endif
-                         ashort |= VOLPBIT_ATTR_NONETIDS;
+ #endif
++                        ashort |= VOLPBIT_ATTR_NONETIDS;
                      if (obj->afp_version >= 32) {
                          if (vol->v_vfs_ea)
+                             ashort |= VOLPBIT_ATTR_EXT_ATTRS;
